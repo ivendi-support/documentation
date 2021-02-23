7f5e6cd8415e7f2daad38313c74325acaafffd5d
Callum Teesdale <callumjamesteesdale@gmail.com>
2021-02-23 12:47:20 +0000

    Quote response

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     'Quoteware': ['GettingStartedQuoteware', 'Quoteware/QuotewareParameters','Quoteware/ExampleRequest', 'Quoteware/ResponseDefinitions', 'Quoteware/PaymentSearch', 'Quoteware/CreditTiers', 'Quoteware/OpenApi', 'Quoteware/Widget','Quoteware/doc1',],
     'Call To Action': ['CTA/CFC', 'CTA/Apply', 'CTA/Reserve', 'CTA/Custom'],
-    'Videos': ['Videos/HP', 'Videos/PCP'],
+    Videos: ['Videos/HP'],
     'Stock Feed': ['Stockfeed/Stockfeed'],
     'Engage': ['Engage/Engage'],
 
