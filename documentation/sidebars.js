cab3f72fcc9be6e2c46959cd0a43b4955fd67a85
Callum Teesdale <callumjamesteesdale@gmail.com>
2021-03-09 16:54:09 +0000

    Most of the documentation has been added. Just polishing needed.

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
   someSidebar: {
-    'Quoteware': ['GettingStartedQuoteware', 'Quoteware/QuotewareParameters','Quoteware/ExampleRequest', 'Quoteware/ResponseDefinitions', 'Quoteware/ExampleQuotewareResponse', 'Quoteware/PaymentSearch', 'Quoteware/CreditTiers', 'Quoteware/OpenApi', 'Quoteware/Widget','Quoteware/doc1',],
+    'Quoteware': ['GettingStartedQuoteware', 'Quoteware/QuotewareParameters','Quoteware/ExampleRequest', 'Quoteware/ResponseDefinitions', 'Quoteware/ExampleQuotewareResponse', 'Quoteware/PaymentSearch', 'Quoteware/CreditTiers',  'Quoteware/Widget','Quoteware/NewVehicle', 'Quoteware/OpenApi'],
     'Call To Action': ['CTA/CFC', 'CTA/Apply', 'CTA/Reserve', 'CTA/Custom'],
-    Videos: ['Videos/HP'],
+    'Videos': ['Videos/HP'],
     'Stock Feed': ['Stockfeed/Stockfeed'],
     'Engage': ['Engage/Engage'],
 
